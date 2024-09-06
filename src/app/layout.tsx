21bf388a851c8f8a22472787207ab70a8f0cce69
whosysAndre <yellowcata2121@gmail.com>
2024-09-06 18:03:11 -0500

    data table

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import {Montserrat_Alternates} from "next/font/google"
+import { Montserrat_Alternates } from "next/font/google"
 import "./globals.css";
 
-const generalFont = Montserrat_Alternates({weight:["400","700"],subsets:["latin"]})
+const generalFont = Montserrat_Alternates({ weight: ["400", "700"], subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,9 +17,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${generalFont.className} antialiased`}
+        className={`${generalFont.className} antialiased `}
       >
-        {children}
+        <header className="w-full bg-violet-900 py-10">
+          <h1 className="text-center capitalize text-3xl text-white">Aministrador básico de gastos</h1>
+        </header>
+
+        <main className="container mx-auto">
+          {children}
+        </main>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,29 @@
+import { columns, Payment } from "@/components/payments/column";
+import { DataTable } from "@/components/payments/data-table";
 
-export default function Home() {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      monto: 100,
+      descripcion: "Comida",
+      fechaPago: "2022-10-2"
+    },
+    // ...
+  ]
+}
+
+export default async function Home() {
+
+  const data = await getData()
 
   return (
     <>
+
+      <div className="container mx-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+
     </>
   );
 }
